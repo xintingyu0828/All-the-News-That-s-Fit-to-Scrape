16ec2706e9e82912e5d3ebf6b9ecf189bb5df247
Xinting Yu <xintingyu@Xintings-MacBook-Pro.local>
2018-09-30 11:37:12 -0500

    load

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# All-the-News-That-s-Fit-to-Scrape
+# Homework 18 - All the News That's Fit to Scrape
+
+This is a Node/Express/Mongo app using Mongoose, Handlebars, and Bootstrap that scrapes news.ycombinator.com and allows users to: `dismiss an article`, `save an article`, `add a note to a saved article`, `delete a note`, and `delete a saved article`.
+
+### Technologies Used
+* Node.js
+* Express.js
+* MongoDB
+* Mongoose ORM
+* Bootsrap
+* Handlebars
+* Heroku, mLab
---
+++ models/note.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const mongoose = require('mongoose'),
+      uniqueValidator = require('mongoose-unique-validator');
+
+// create Schema class
+const Schema = mongoose.Schema;
+
+// create note schema
+const NoteSchema = new Schema({
+  // title is a required string
+  text: {
+    type: String,
+    required: true
+  },
+  // date is set when added to database
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// add unique-validator plugin
+NoteSchema.plugin(uniqueValidator);
+
+// create the Note model with the NoteSchema
+const Note = mongoose.model("Note", NoteSchema);
+
+// export the model
+module.exports = Note;
